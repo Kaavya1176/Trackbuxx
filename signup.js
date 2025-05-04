5548a033a4efd7dc899d06227be7542d4fef6db5
Gurleen Saini <196659986+gurleen1166@users.noreply.github.com>
2025-05-04 10:51:37 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -2,12 +2,9 @@ const wrapper= document.querySelector(".wrapper");
 const registerlink= document.querySelector(".register-link");
 const loginlink= document.querySelector(".login-link");
 
-
 registerlink.onclick =() => {
     wrapper.classList.add('active');
 }
-
-
 loginlink.onclick =() => {
     wrapper.classList.remove('active');
 }
